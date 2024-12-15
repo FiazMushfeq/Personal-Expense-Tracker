7212513e02572e14ede0389fab0e68031a8d6fd3
Fiaz Mushfeq <38337054+FiazMushfeq@users.noreply.github.com>
2024-12-14 20:34:21 -0500

    finished form

--- frontend/lib/forms/expense_form.dart
+++ frontend/lib/forms/expense_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 class ExpenseForm extends StatefulWidget {
   const ExpenseForm({super.key});
@@ -10,6 +11,16 @@ class ExpenseForm extends StatefulWidget {
 
 class _ExpenseFormState extends State<ExpenseForm> {
   final _formKey = GlobalKey<FormState>();
+  final TextEditingController _dateController = TextEditingController();
+  final List<String> _dropdownItems = [
+    'UNKNOWN',
+    'FOOD',
+    'TRANSPORT',
+    'SHOPPING',
+    'ENTERTAINMENT'
+  ];
+  late DateTime _selectedDate;
+  String? _selectedItem;
   String _title = '';
   double _amount = 0;
 
@@ -38,27 +49,70 @@ class _ExpenseFormState extends State<ExpenseForm> {
                 },
               ),
               TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Amount',
-                    hintText: 'Ex: 50.00',
-                  ),
-                  keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
-                  ],
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter amount';
-                    }
-                    final double? parsedValue = double.tryParse(value);
-                    if (parsedValue == null) {
-                      return 'Please enter appropriate amount';
-                    }
-                    return null;
-                  },
-                  onSaved: (value) {
-                    _amount = value as double;
-                  }),
+                decoration: const InputDecoration(
+                  labelText: 'Amount',
+                  hintText: 'Ex: 50.00',
+                ),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*')),
+                ],
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter amount';
+                  }
+                  final double? parsedValue = double.tryParse(value);
+                  if (parsedValue == null) {
+                    return 'Please enter appropriate amount';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _amount = value as double;
+                },
+              ),
+              const SizedBox(height: 16.0),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  labelText: 'Category',
+                  border: OutlineInputBorder(),
+                ),
+                value: _selectedItem,
+                items: _dropdownItems.map((String category) {
+                  return DropdownMenuItem(
+                      value: category, child: Text(category));
+                }).toList(),
+                onChanged: (String? newValue) {
+                  setState(() {
+                    _selectedItem = newValue;
+                  });
+                },
+                validator: (value) =>
+                    value == null ? 'Please select a category' : null,
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Select Date',
+                  border: OutlineInputBorder()
+                ),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2101)
+                  );
+                  if(pickedDate != null) {
+                    setState(() {
+                      _selectedDate = pickedDate;
+                      _dateController.text = DateFormat('yyyy-MM-dd').format(_selectedDate);
+                    });
+                  }
+                },
+              ),
               const SizedBox(height: 16.0),
               ElevatedButton(
                 onPressed: () {
